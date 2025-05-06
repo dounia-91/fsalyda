3dcde57542d34881f0aff831e54828e2d139bb0e
dounia-91 <doubnts@gmail.com>
2025-05-06 01:53:22 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
@@ -21,6 +23,12 @@ const nextConfig = {
       bodySizeLimit: "10mb",
     },
   },
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
+
