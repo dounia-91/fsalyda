a331cfdcbfff2c4147ced3c82a97dc2f4e07a468
dounia-91 <doubnts@gmail.com>
2025-05-06 02:33:48 +0100

    Update layout.tsx

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,28 +1,26 @@
 "use client";
 import Sidebar from "@/components/sidebar/sidebar";
 import { useState, useEffect } from "react";
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
   // Enregistrer le service worker au moment où le composant est monté
   useEffect(() => {
+    // Vérifie si le service worker est supporté par le navigateur
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
         .register("/service-worker.js")
         .then((registration) => {
-          console.log("Service Worker enregistré avec succès:", registration);
+          console.log("Service Worker enregistré avec succès :", registration);
         })
         .catch((error) => {
-          console.log("Échec de l'enregistrement du Service Worker:", error);
+          console.log("Échec de l'enregistrement du Service Worker :", error);
         });
     }
   }, []);
-
   return (
     <main className="relative w-full h-[calc(100vh-theme(space.20))] flex">
       <div
