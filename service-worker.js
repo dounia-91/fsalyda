cd195ead8a3c431aaee13de837194cd91aabf447
dounia-91 <doubnts@gmail.com>
2025-05-06 02:11:38 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+// service-worker.js
+
+const CACHE_NAME = "fsalyda-cache-v1";
+const urlsToCache = [
+  "/",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/screenshot1.png",
+  "/screenshot2.png",
+  "/adminDashboard",
+];
+
+// Installation du service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activation du service worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepter les requêtes et les servir depuis le cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
