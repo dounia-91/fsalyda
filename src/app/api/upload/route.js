e4b4128f9ac7dcca35353f0ab7b7548da58f15ba
dounia-91 <doubnts@gmail.com>
2025-05-17 05:23:23 +0100

    Update route.js

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -5,8 +5,8 @@ import { NextResponse } from 'next/server';
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY!,
-  api_secret: process.env.CLOUDINARY_API_SECRET!,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Fonction pour convertir un ReadableStream en Buffer
