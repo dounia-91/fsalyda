d8c9456b195fbb670a32f72edb45740178a4ba13
dounia-91 <doubnts@gmail.com>
2025-05-18 08:59:26 +0100

    Update previewFilledForm.tsx

--- src/components/previewFilledForm.tsx
+++ src/components/previewFilledForm.tsx
@@ -23,6 +23,7 @@ type Props = {
   formToFillTitle: string;
   setFormToFillTitle: React.Dispatch<React.SetStateAction<string>>;
 };
+
 export default function PreviewFilledForm({
   showPreviewModal,
   setShowPreviewModal,
@@ -33,50 +34,32 @@ export default function PreviewFilledForm({
   setFormToFillTitle,
 }: Props) {
   const [formState, setFormState] = useState<FormState>({});
+
   useEffect(() => {
-    // const initState: FormState = {};
-    // formItemDetails.map((itemD) => {
-    //   if (itemD.title === "Date & Time") {
-    //     initState[itemD.newTitle] = recordState[itemD.newTitle];
-    //   } else if (itemD.title === "Check Box") {
-    //     initState[itemD.newTitle] = itemD.checkBoxDefaultValue!;
-    //   } else if (itemD.title === "List") {
-    //     if (itemD.listMultipleSelection) {
-    //       initState[itemD.newTitle] = itemD.listMulDefaultValue!;
-    //     } else {
-    //       initState[itemD.newTitle] = itemD.listDefaultValue!;
-    //     }
-    //   } else if (itemD.title === "Choice") {
-    //     initState[itemD.newTitle] = itemD.listDefaultValue!;
-    //   } else if (itemD.title === "Attached file") {
-    //     initState[itemD.newTitle] = [];
-    //   } else if (itemD.title === "Photo") {
-    //     initState[itemD.newTitle] = [];
-    //   } else if (itemD.title === "Voice Recorder") {
-    //     initState[itemD.newTitle] = [];
-    //   } else if (itemD.title === "Image") {
-    //     initState[itemD.newTitle] = itemD.imageFileNames!;
-    //   } else if (itemD.title === "Calculation") {
-    //     initState[itemD.newTitle] = "0";
-    //   } else if (itemD.title === "Table") {
-    //     const emptyArray = Array.from(
-    //       { length: itemD.tableMaxRows! },
-    //       (i: number) => [
-    //         i + 1,
-    //         ...new Array(itemD.tableCols!.length - 1).fill(""),
-    //       ]
-    //     );
-    //     emptyArray.forEach((row, index) => {
-    //       row[0] = index + 1;
-    //     });
-    //     initState[itemD.newTitle] = emptyArray;
-    //     initState[`${itemD.newTitle}RowCount`] = "1";
-    //   } else {
-    //     initState[itemD.newTitle] = "";
-    //   }
-    // });
-    setFormState(recordState);
+    const initState: FormState = {};
+
+    formItemDetails.forEach((itemD) => {
+      const value = recordState[itemD.newTitle];
+
+      if (itemD.title === "Voice Recorder" && Array.isArray(value)) {
+        // Convert Blob[] en URL audio
+        initState[itemD.newTitle] = URL.createObjectURL(
+          new Blob(value, { type: "audio/webm;codecs=opus" })
+        );
+      } else if (itemD.title === "Photo" && Array.isArray(value)) {
+        // Convertir chaque fichier image en URL
+        initState[itemD.newTitle] = value.map((file: File | Blob | string) =>
+          typeof file === "string" ? file : URL.createObjectURL(file)
+        );
+      } else {
+        // Pour les autres types, garder la valeur telle quelle
+        initState[itemD.newTitle] = value;
+      }
+    });
+
+    setFormState(initState);
   }, [formItemDetails, recordState]);
+
   return (
     <div
       className={`${
@@ -87,147 +70,39 @@ export default function PreviewFilledForm({
         <h1 className="w-full text-center text-2xl font-bold">
           {formToFillTitle}
         </h1>
+
         {formItemDetails.map((itemD, index) => {
-          if (itemD.title === "Input field") {
-            return (
-              <RenderInputField
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "Text Area") {
-            return (
-              <RenderTextArea
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "Date & Time") {
-            return (
-              <RenderDateTime
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "Check Box") {
-            return (
-              <RenderCheckbox
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "List") {
-            return (
-              <RenderList
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "Choice") {
-            return (
-              <RenderChoice
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "Photo") {
-            return (
-              <RenderPhoto
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "Voice Recorder") {
-            return (
-              <RenderVoiceRecorder
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "Attached file") {
-            return (
-              <RenderAttachedFile
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "Table") {
-            return (
-              <RenderTable
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                preview={true}
-              />
-            );
-          }
-          if (itemD.title === "Image") {
-            return <RenderImage key={index} itemD={itemD} preview={true} />;
-          }
-          if (itemD.title === "Calculation") {
-            return (
-              <RenderCalculation
-                key={index}
-                itemD={itemD}
-                formState={formState}
-                setFormState={setFormState}
-                formItemDetails={formItemDetails}
-                preview={true}
-              />
-            );
-          }
-        })}
-        <div className="w-full flex items-center justify-center text-white">
-          <button
-            type="button"
-            className="bg-red-500 p-2 rounded-lg cursor-pointer"
-            onClick={() => {
-              setFormToFillTitle("");
-              setFormItemDetails([]);
-              setShowPreviewModal(false);
-            }}
-          >
-            Close
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
+          switch (itemD.title) {
+            case "Input field":
+              return (
+                <RenderInputField
+                  key={index}
+                  itemD={itemD}
+                  formState={formState}
+                  setFormState={setFormState}
+                  preview={true}
+                />
+              );
+            case "Text Area":
+              return (
+                <RenderTextArea
+                  key={index}
+                  itemD={itemD}
+                  formState={formState}
+                  setFormState={setFormState}
+                  preview={true}
+                />
+              );
+            case "Date & Time":
+              return (
+                <RenderDateTime
+                  key={index}
+                  itemD={itemD}
+                  formState={formState}
+                  setFormState={setFormState}
+                  preview={true}
+                />
+              );
+            case "Check Box":
+              return (
+                <RenderCheckbox
